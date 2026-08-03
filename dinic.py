285ce41a502d298c038dd5491dccfc5a8fd84941
kinstugi <kinstugi.webdev@gmail.com>
2026-08-04 00:39:32 +0200

    implemented edge class, about to create flow graph

---
+++ dinic.py
@@ -0,0 +1,3 @@
+class DinicsMaxFlow:
+    def __init__(self):
+        pass
--- graph_lib/edge.py
+++ graph_lib/edge.py
@@ -3,6 +3,16 @@ from graph_lib import Node
 
 class Edge:
     def __init__(self, node_a: Node, node_b: Node, max_cap: int = 1):
-        self.node_a = node_a
-        self.node_b = node_b
-        self.edge_cap = max_cap
+        self.from_node = node_a
+        self.to_node = node_b
+        self.max_cap = max_cap
+        self.cap = 0
+        self.rev: Edge | None = None
+    
+    def get_remaining_cap(self) -> int:
+        return self.max_cap - self.cap
+    
+    def augment_path(self, val: int) -> None:
+        self.cap += val
+        if self.rev:
+            self.rev.cap -= val
