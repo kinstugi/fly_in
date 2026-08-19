40722bfb18456c77abfd8c64742c84c10161d85b
Baffour Kusi Frimpong <bkusi-fr@3-A-6.42heilbronn.de>
2026-08-19 02:17:20 +0200

    fixed a logical bug with solution engine

--- solution_engine.py
+++ solution_engine.py
@@ -3,8 +3,9 @@ from dinic_teg_solver import DincTEGSolver
 
 
 class SolutionEngine:
-    def __init__(self, graph: StaticGraph):
+    def __init__(self, graph: StaticGraph, num_drones: int):
         self.graph = graph
+        self.num_drones = num_drones
 
     def find_optimal_time(self) -> int:
         low, high = 1, 80
@@ -16,7 +17,7 @@ class SolutionEngine:
             dinic = DincTEGSolver(teg)
             max_flow = dinic.get_max_flow()
 
-            if max_flow >= mid:
+            if max_flow >= self.num_drones:
                 ans = mid
                 high = mid - 1
             elif max_flow < mid:
