724a356399aa3dd6f15aa9ab6e15ad94fc69a664
Baffour Kusi Frimpong <bkusi-fr@3-A-6.42heilbronn.de>
2026-08-19 03:58:22 +0200

    renamed a function in dinic teg solver

--- dinic_teg_solver.py
+++ dinic_teg_solver.py
@@ -1,13 +1,15 @@
 from graph_lib import TEG, Node
+from collections import deque
+
 
 class DincTEGSolver:
     def __init__(self, graph: TEG):
-        pass
+        self.teg = graph
 
     def build_level_graph(self) -> dict[Node, int]:
         return {}
 
-    def find_and_augment_flow_path(self) -> int:
+    def push_flow(self) -> int:
         return 0
 
     def get_max_flow(self) -> int:
