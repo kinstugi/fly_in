0539a1a0d0cf1437699b195773eaf7f4a062a0ae
Baffour Kusi Frimpong <bkusi-fr@3-I-4.42heilbronn.de>
2026-08-20 08:07:41 +0200

    teg almost there

--- graph_lib/time_extended_graph.py
+++ graph_lib/time_extended_graph.py
@@ -7,7 +7,7 @@ class TimeExtendedGraph:
     def __init__(self, turns: int, s_graph: StaticGraph):
         self.turns = turns
         self.graph: dict[TNode, list[TEdge]] = defaultdict(list)
-        self.super_source: list[TNode] = []
+        self.super_source = TNode(s_graph.source_node, 0, )
         self.super_sink: list[TNode] = []
         self.build_graph(s_graph)
 
@@ -25,3 +25,15 @@ class TimeExtendedGraph:
                     edges.append(TEdge(u_in_t, u_out_t, sys.maxsize))
                 else:
                     edges.append(TEdge(u_in_t, u_out_t, u_in_t.node.max_drones))
+
+        for s_node in s_graph.graph.keys():
+            for edge in s_graph.graph[s_node]:
+                for t in range(self.turns):
+                    from_node = TNode(edge.from_node, t, Role.r_out)
+                    to_node = TNode(edge.to_node, t+1, Role.r_in)
+                    edges.append(TEdge(from_node, to_node, edge.max_link_cap))
+
+            for t in range(self.turns):
+                from_node = TNode(s_node, t, Role.r_out)
+                to_node = TNode(s_node, t+1, Role.r_in)
+                edges.append(TEdge(from_node, to_node, s_node.max_drones))
