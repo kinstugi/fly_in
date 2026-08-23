2867c70ce5d77735c32a2ed48dfddac9af3f6157
kinstugi <kinstugi.webdev@gmail.com>
2026-08-23 02:31:45 +0200

    implemented level graph

--- dinic_teg_solver.py
+++ dinic_teg_solver.py
@@ -1,4 +1,4 @@
-from graph_lib import TEG, Node
+from graph_lib import TEG, TNode
 from collections import deque
 
 
@@ -6,8 +6,28 @@ class DincTEGSolver:
     def __init__(self, graph: TEG):
         self.teg = graph
 
-    def build_level_graph(self) -> dict[Node, int]:
-        return {}
+    def build_level_graph(self) -> dict[TNode, int]:
+        level_graph: dict[TNode, int] = {}
+        q: deque[TNode] = deque([self.teg.source_node])
+        level_graph[self.teg.source_node] = 0
+
+        while q:
+            cnt = len(q)
+            done = False
+
+            for _ in range(cnt):
+                nd = q.popleft()
+                if nd == self.teg.sink_node:
+                    done = True
+                for edge in self.teg.graph[nd]:
+                    if level_graph.get(edge.to_node, -1) != -1 or edge.get_remaining_flow() < 1:
+                        continue
+                    q.append(edge.to_node)
+                    level_graph[edge.to_node] = level_graph[nd] + 1
+            if done:
+                break
+                
+        return level_graph
 
     def push_flow(self) -> int:
         return 0
